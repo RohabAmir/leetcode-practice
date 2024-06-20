bf2fb30cb9b639d726aa90aa70ebabd348e685bc
RohabAmir <rohabamir2@gmail.com>
2024-06-21 01:13:24 +0500

    Binary Search Algo

---
+++ CountOccurance.js
@@ -0,0 +1,25 @@
+// I have an array such like : arr=['a','a',b',c','c','c'] write the js function in best optimal code suhc that we should make an obj from that array where key is the value of arary and value of obj is the occurances of value in that array such that:
+// obj:{ "a":2, "b":1, "c":3 }
+
+
+function countOccurrences(arr) {
+    const result = {};  // Initialize an empty object to store the counts
+
+    for (let i = 0; i < arr.length; i++) {  // Loop through the array
+        const item = arr[i]; // get the fisrt element of aarray 
+
+        if (result[item]) {
+            result[item] += 1;  // If the item is already in the object, increment its count
+        } else {
+            result[item] = 1;  // If the item is not in the object, add it with a count of 1
+        }
+
+    }
+
+    return result;  // Return the resulting object with counts
+}
+
+// Example usage
+const arr = ['a', 'a', 'b', 'c', 'c', 'c'];
+const occurrences = countOccurrences(arr);
+console.log(occurrences);
